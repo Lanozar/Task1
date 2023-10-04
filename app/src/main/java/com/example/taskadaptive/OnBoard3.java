b922844c032c07d804e48c3434b45e2ce735da20
rmp <asdasdasd>
2023-10-04 18:08:09 +0300

    ura

---
+++ app/src/main/java/com/example/taskadaptive/OnBoard3.java
@@ -0,0 +1,15 @@
+package com.example.taskadaptive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoard3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board3);
+    }
+
+}
